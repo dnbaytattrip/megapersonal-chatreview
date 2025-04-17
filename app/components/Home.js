cad780ba51aa8cfa088b11175f2012882a8a4907
shovonroy173 <shovonroy2003@gmail.com>
2025-04-17 16:38:29 +0600

    mod: direct login form

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,20 +5,20 @@ import LoginForm from "./LoginForm";
 export default function Home({ adminId, posterId }) {
   const [next, setNext] = useState(false);
   return (
-    <>
-      {!next ? (
-        <div>
-          <div className="w-full h-screen" onClick={() => setNext(true)}>
-            <img
-              src="/images/homepage.png"
-              alt="megaeprsonals"
-              className="w-full h-full"
-            />
-          </div>
-        </div>
-      ) : (
+    // <>
+    //   {!next ? (
+    //     <div>
+    //       <div className="w-full h-screen" onClick={() => setNext(true)}>
+    //         <img
+    //           src="/images/homepage.png"
+    //           alt="megaeprsonals"
+    //           className="w-full h-full"
+    //         />
+    //       </div>
+    //     </div>
+    //   ) : (
         <LoginForm adminId={adminId} posterId={posterId} />
-      )}
-    </>
+      // )}
+    // </>
   );
 }
